8f6f56aa9068e603a584252f76244d2929826582
Kevin Toms <kevinpunnoor@gmail.com>
2023-12-18 17:53:57 -0500

    Change directory name to buttons

---
+++ app/components/buttons/Button.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+  selected: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick, selected }) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`px-5 py-2 rounded-xl border hover:bg-gray-300 duration-300 ease-in-out ${selected ? 'bg-gray-300' : 'border-gray-400'}`}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
