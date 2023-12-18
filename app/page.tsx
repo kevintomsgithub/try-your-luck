394f5c0b3a4ab70591e06319938cd3b6c1702915
Kevin Toms <kevinpunnoor@gmail.com>
2023-12-18 13:47:43 -0500

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/navbar/Navbar";
 import Settings from "./components/settings/Settings";
 import Game from "./components/game/Game";
+import LoadingBar from "./components/loading/Loading";
+import BidComponent from "./components/bid/Bid";
 
 import Image from "next/image";
 
@@ -8,8 +10,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Navbar />
+      {/* <LoadingBar /> */}
       <Settings />
       <Game />
+      <BidComponent />
     </main>
   );
 }
