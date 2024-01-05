983553be9dc43a3af0245798ac31c585436ae437
Kevin Toms <kevinpunnoor@gmail.com>
2024-01-04 20:55:29 -0500

    Add Footer to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import Game from "./components/game/Game";
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Navbar />
       <Game />
+      <Footer />
     </main>
   );
 }
