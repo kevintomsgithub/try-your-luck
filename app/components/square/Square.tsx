bf972dfc85109de472e6cdaa105b419534973d37
Kevin Toms <kevinpunnoor@gmail.com>
2024-01-04 20:53:32 -0500

    Reduce square size

--- app/components/square/Square.tsx
+++ app/components/square/Square.tsx
@@ -7,7 +7,7 @@ const SquareComponent = ({ bgColorClass, onSelect, selected }) => {
 
   return (
     <div
-      className={`w-36 h-36 flex items-center justify-center text-center rounded-2xl drop-shadow-md duration-300 ease-in-out hover:drop-shadow-2xl cursor-pointer hover:scale-105 ${bgColorClass} ${
+      className={`w-28 h-28 flex items-center justify-center text-center rounded-2xl drop-shadow-md duration-300 ease-in-out hover:drop-shadow-2xl cursor-pointer hover:scale-105 ${bgColorClass} ${
         selected ? "ring-offset-2 ring-4 ring-yellow-500" : ""
       }`}
       onClick={handleClick}
